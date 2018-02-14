d0c3c21ef6893970847001f10c9596fee814d317
Joe Nelson <cred+github@begriffs.com>
2018-02-14 01:34:56 -0600

    handles biggest bigint values

--- sql/pg_rational_test.sql
+++ sql/pg_rational_test.sql
@@ -2,14 +2,16 @@ create extension pg_rational;
 
 -- input
 
--- Can parse a simple fraction
+-- can parse a simple fraction
 select '1/3'::rational;
--- Interprets negative numerator
+-- interprets negative numerator
 select '-1/3'::rational;
--- Moves negative value from denom to numer
+-- moves negative value from denom to numer
 select '1/-3'::rational;
--- Double negative becomes positive
+-- double negative becomes positive
 select '-1/-3'::rational;
+-- biggest values
+select '9223372036854775807/9223372036854775807'::rational;
 -- no spaces
 select '1 /3'::rational;
 -- no single numbers
