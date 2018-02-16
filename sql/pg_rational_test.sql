700a38353ffdfe211acf6b893a085f8d64f7ceb0
Joe Nelson <cred+github@begriffs.com>
2018-02-15 22:08:15 -0600

    Test simplification

--- sql/pg_rational_test.sql
+++ sql/pg_rational_test.sql
@@ -4,16 +4,9 @@ create extension pg_rational;
 
 -- can parse a simple fraction
 select '1/3'::rational;
--- interprets negative numerator
+-- can parse negatives
 select '-1/3'::rational;
--- moves negative value from denom to numer
 select '1/-3'::rational;
--- don't move negative if it would overflow
-select '1/-9223372036854775808'::rational;
--- double negative becomes positive
-select '-1/-3'::rational;
--- biggest values
-select '9223372036854775807/9223372036854775807'::rational;
 
 -- SEND works
 select rational_send('1/3');
@@ -29,6 +22,19 @@ select '1'::rational;
 -- no garbage
 select ''::rational;
 select 'sdfkjsdfj34984538'::rational;
+
+-- simplification
+
+-- double negative becomes positive
+select rational_simplify('-1/-3');
+-- don't move negative if it would overflow
+select rational_simplify('1/-9223372036854775808');
+-- biggest value reduces
+select rational_simplify('9223372036854775807/9223372036854775807');
+-- smallest value reduces
+select rational_simplify('-9223372036854775808/-9223372036854775808');
+-- idempotent on simplified expression
+select rational_simplify('1/1');
 
 -- addition
 
