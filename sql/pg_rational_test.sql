45e077f716a669a05587bfcfebb531bba4b7e044
Andreas Degert <ad@papyrus-gmbh.de>
2019-11-16 10:04:27 +0100

    Add missing overflow checks to rational_in_float()
    Overflow leads to wrong results and can create non-terminating loops
    in the postgresql backend.
    
    Changes:
    
     - return ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE when input is too large
       to be represented as rational
    
     - end refinement when the resulting numerator or denominator would
       not fit into an int32.
    
     - added some test cases

--- sql/pg_rational_test.sql
+++ sql/pg_rational_test.sql
@@ -23,6 +23,13 @@ select 0.263157894737::float::rational;
 select 3.141592625359::float::rational;
 select 0.606557377049::float::rational;
 select -0.5::float::rational;
+select 1.000001::float::rational;
+select 1.0000001::float::rational;
+select 1.00000001::float::rational;
+select 1.000000001::float::rational;
+select 1.0000000001::float::rational;
+select 2147483647::float::rational;
+select 2147483647.1::float::rational;
 
 -- to float
 select '1/2'::rational::float;
